e99d1b9bee5aaf7854fdd13a64d0b8a948860783
taeho <dlxogh439@gmail.com>
2024-08-25 22:25:35 +0900

    Edit 화면에 Bean Validation 추가

--- src/main/java/hello/itemservice/web/validation/ItemValidator.java
+++ src/main/java/hello/itemservice/web/validation/ItemValidator.java
@@ -26,12 +26,5 @@ public class ItemValidator implements Validator {
         if (item.getQuantity() == null || item.getQuantity() >= 9999) {
             errors.rejectValue("quantity", "max", new Object[]{9999}, null);
         }
-        if (item.getPrice() != null && item.getQuantity() != null) {
-            int resultPrice = item.getPrice() * item.getQuantity();
-            if (resultPrice < 10000) {
-                errors.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
-            }
-        }
-        
     }
 }
